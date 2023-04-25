6e6f71df7e67a4a9f2756236926d7729805f108b
Konish Dutta <konishdutta@gmail.com>
2023-04-24 19:32:16 -0700

    Flik

--- lab4/flik/FlikTest.java
+++ lab4/flik/FlikTest.java
@@ -1,2 +1,30 @@
-package flik;public class FlikTest {
+package flik;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class FlikTest {
+    @Test
+    public void testOne() {
+        int x = 128;
+        int y = 128;
+        assertTrue(Flik.isSameNumber(x, y));
+    }
+
+    @Test
+    public void testTwo() {
+        int x = 127;
+        int y = 127;
+        assertTrue(Flik.isSameNumber(x, y));
+    }
+
+    @Test
+    public void HorribleSteve() {
+        int i = -10000;
+
+        for (int j = -10000; i < 10000; ++i, ++j) {
+            System.out.println(i + " " + j);
+            assertTrue(Flik.isSameNumber(i, j));
+        }
+    }
+
 }
